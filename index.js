e35062bdb5462d206eb6fff0e0808189906e9de5
George Thayamkery <george.thayamkery@gmail.com>
2016-11-22 15:51:43 -0600

    11/22/16

--- index.js
+++ index.js
@@ -1,30 +1,33 @@
 // View information about Express JS and its server side capabilities @ https://expressjs.com/en/guide/routing.html
 var express = require('express');
+var firebase = require('firebase');
 var app = express();
 
 //Templating engine: EJS
 //Documentation: https://github.com/tj/ejs
 app.set('view engine', 'ejs')
 
-app.get('/', function (req, res) {
-	// host is the incoming url
-	// changes depending on subdomain
-	// ex) cs.reedyhighschool.com  or  deca.reedyhighschool.com
+app.use(express.static('static'))
 
-	var host = req.headers.host;
-	switch(host){
-	    case "reedyhighschool.com":
-			res.render("index")
-			break;
-		case "www.reedyhighschool.com":
-			res.render("index")
-			break;
-	    default:
-	    	res.render("clubnotfound", {"host":host});
-	    	break;
-	}
+app.get('/', function(req, res) {
+    // host is the incoming url
+    // changes depending on subdomain
+    // ex) cs.reedyhighschool.com  or  deca.reedyhighschool.com
+
+    var host = req.headers.host;
+    switch (host) {
+        case "reedyhighschool.com":
+            res.render("index")
+            break;
+        case "www.reedyhighschool.com":
+            res.render("index")
+            break;
+        default:
+            res.render("clubnotfound", { "host": host });
+            break;
+    }
 });
 
-app.listen(10001, function () {
-	console.log('Example app listening on port 10001!\nType http://localhost:10001/ in your web browser to see the Hello World page.');
+app.listen(10001, function() {
+    console.log('Example app listening on port 10001!\nType http://localhost:10001/ in your web browser to see the Hello World page.');
 });
